b7f06dc2de523e26e57fa6977fa1b2a563942199
Haruki Kurokawa <ha.brack96@gmail.com>
2023-09-20 22:22:32 +0900

    [update] hashTagsをlocal storageに対応

--- src/components/App.js
+++ src/components/App.js
@@ -8,24 +8,28 @@ function App() {
     localStorage.getItem('webSites') ? JSON.parse(localStorage.getItem('webSites')) : []
   );
   const [hashTags, setHashTags] = useState(
-    [{name: "hoge", count: 1}, {name: "fuga", count: 3}/*{ name: '', count: 0 }*/]
+    localStorage.getItem('hashTags') ? JSON.parse(localStorage.getItem('hashTags')) : []
+    /* [...{ name: '', count: 0 }] */
   );
 
   function increaseHashTag(name) {
+    let newHashTags;
     if (hashTags.find((hashTag) => hashTag.name === name)) {
-        // name is already registered.
-        setHashTags(hashTags.map((hashTag) => 
-            hashTag.name === name ? 
-                { name: name, count: hashTag.count + 1} :
-                hashTag
-        ))
+      // name is already registered.
+      newHashTags = hashTags.map((hashTag) => 
+        hashTag.name === name ?
+          { name: name, count: hashTag.count + 1} :
+          hashTag
+      );
     } else {
-        // register new name hashtag.
-        setHashTags([
-            { name: name, count: 1 },
-            ...hashTags
-        ]);
+      // register new name hashtag.
+      newHashTags = [
+        { name: name, count: 1 },
+        ...hashTags
+      ]
     }
+    setHashTags(newHashTags);
+    localStorage.setItem('hashTags', JSON.stringify(newHashTags));
   }
 
   function decreaseHashTag(name) {
@@ -38,6 +42,7 @@ function App() {
     });
     newHashTags = newHashTags.filter((hashTag) => hashTag.count > 0);
     setHashTags(newHashTags);
+    localStorage.setItem('hashTags', JSON.stringify(newHashTags));
   }
 
   function addWebSite(webSite) {
