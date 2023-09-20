dbd98df57be6668c911443ee4416155306fd21e1
Haruki Kurokawa <ha.brack96@gmail.com>
2023-09-20 21:09:27 +0900

    [update] Local Storageに対応

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import Register from './Register';
+import WebSiteList from './WebSiteList';
 import { useState } from 'react';
 
 function App() {
-  const [webSites, setWebSites] = useState([]);
+  const [webSites, setWebSites] = useState(
+    localStorage.getItem('webSites') ? JSON.parse(localStorage.getItem('webSites')) : []
+  );
   const [hashTags, setHashTags] = useState(
     [{name: "hoge", count: 1}, {name: "fuga", count: 3}/*{ name: '', count: 0 }*/]
   );
@@ -24,13 +27,26 @@ function App() {
     }
   }
 
+  function addWebSite(webSite) {
+    const newWebSites = [webSite, ...webSites];
+    setWebSites(newWebSites);
+    localStorage.setItem('webSites', JSON.stringify(newWebSites));
+  }
+
+  function deleteWebSite(webSite) {
+    const newWebSites = webSites.filter((web) => web !== webSite);
+    setWebSites(newWebSites);
+    localStorage.setItem('webSites', JSON.stringify(newWebSites));
+  }
+
   return (
     <div>
       <Register
-        registerWebSite={(webSite) => setWebSites([...webSites, webSite])}
+        registerWebSite={addWebSite}
         hashTags={hashTags}
         processHashTag={processHashTag}
       />
+      <WebSiteList webSites={webSites} deleteWebSite={deleteWebSite}/>
     </div>
   );
 }
