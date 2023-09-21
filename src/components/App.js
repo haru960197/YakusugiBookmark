5cf1e0305b3fb7470efd64a2879d1b276db8c8de
Haruki Kurokawa <ha.brack96@gmail.com>
2023-09-21 22:22:38 +0900

    [update] タイトルを追加

--- src/components/App.js
+++ src/components/App.js
@@ -2,43 +2,13 @@ import Register from './Register';
 import WebSiteList from './WebSiteList';
 import HashTagList from './HashTagList';
 import { useState } from 'react';
-import { Grid } from "@mui/material"
+import { Grid, Typography } from "@mui/material"
 
 function App() {
-  /*
   const [webSites, setWebSites] = useState(
     localStorage.getItem('webSites') ? JSON.parse(localStorage.getItem('webSites')) : []
   );
-  */
 
-  const [webSites, setWebSites] = useState(
-    localStorage.getItem('webSite') && JSON.parse(localStorage.getItem('webSite')).length > 0
-      ? JSON.parse(localStorage.getItem('webSite'))
-      : [{
-        url: "https://github.com/haru960197/LatexFigureManager",
-        title: "俺のGitHub",
-        hashTagNames: ["github", "javascript"],
-        siteTitle: "Kuro960197's GitHub",
-        registerDate: "2023-09-21-09",
-        accessCount: 1
-      },
-      {
-        url: "https://mui.com/material-ui/react-autocomplete/",
-        title: "MUI-Autocomplete",
-        hashTagNames: ["javascript", "react", "mui"],
-        siteTitle: 'React Autocomplete component - Material UI',
-        registerDate: '2023-09-20-15',
-        accessCount: 3
-      },
-      {
-        url: "https://www.opengraph.io/pricing",
-        title: "サイトのタイトル取得API",
-        hashTagNames: ["javascript", "react", "api"],
-        siteTitle: 'Simple Pricing | Simple API',
-        registerDate: '2023-09-20-23',
-        accessCount: 2
-      }]
-  );
   const [hashTags, setHashTags] = useState(
     localStorage.getItem('hashTags') ? JSON.parse(localStorage.getItem('hashTags')) : []
     /* [...{ name: '', count: 0, lastUsedDate: '' }] */
@@ -117,6 +87,9 @@ function App() {
     <div>
       <Grid container spacing={4}>
         <Grid item container direction="column" xs={12} sm={4}>
+          <Grid item sx={{ marginTop: 2, marginBottom: 1, mx: 5 }}>
+            <Typography variant='h4'>屋久杉ブックマーク</Typography>
+          </Grid>
           <Grid item>
             <Register
               registerWebSite={addWebSite}
