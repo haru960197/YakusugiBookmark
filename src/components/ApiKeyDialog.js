6bdde90f90a99961588df531cb8240c6257c38bc
Haruki Kurokawa <ha.brack96@gmail.com>
2023-09-23 11:25:25 +0900

    [add]

---
+++ src/components/ApiKeyDialog.js
@@ -0,0 +1,61 @@
+import {
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    TextField,
+    Link,
+    Button
+} from "@mui/material";
+import { useState } from 'react';
+
+export default function ApiKeyDialog({ setApiKey }) {
+    const [open, setOpen] = useState(!Boolean(localStorage.getItem('apiKey')));
+    const [keyInput, setKeyInput] = useState("");
+    const apiKeySiteUrl = "https://www.opengraph.io/";
+
+    function handleRegister() {
+        localStorage.setItem('apiKey', keyInput);
+        setApiKey(keyInput);
+        setKeyInput("");
+        setOpen(false);
+    }
+
+    function handleCancel() {
+        setKeyInput("");
+        setOpen(false);
+    }
+
+    return (
+        <Dialog open={open} onClose={() => setOpen(false)}>
+            <DialogTitle>APIキーの入力</DialogTitle>
+            <DialogContent>
+                <Link
+                    variant="subtitle2"
+                    href={apiKeySiteUrl}
+                    target="_blank"
+                >{apiKeySiteUrl}</Link>
+                <br />
+                <DialogContentText>
+                上記サイトの無料プランで取得できるAPIキーを入力することで、
+                登録したウェブサイトの「サムネイル・ファビコン・タイトル」を取得して表示することができます。
+                </DialogContentText>
+                <TextField
+                    // autoFocus
+                    margin="dense"
+                    label="APIキー"
+                    placeholder="00000000-0000-0000-0000-000000000000"
+                    fullWidth
+                    variant="standard"
+                    value={keyInput}
+                    onChange={(e) => setKeyInput(e.target.value)}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleCancel}>必要ない</Button>
+                <Button onClick={handleRegister}>登録</Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
