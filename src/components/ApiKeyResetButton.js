7ee124753187247fe2e45f6003c4a517309801fd
Haruki Kurokawa <ha.brack96@gmail.com>
2023-09-26 12:23:01 +0900

    [update] レイアウトを調整

--- src/components/ApiKeyResetButton.js
+++ src/components/ApiKeyResetButton.js
@@ -1,18 +1,21 @@
 import { Box } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
 import KeyIcon from '@mui/icons-material/Key';
 
-export default function ApiKeyResetButton({ SetDialogOpen }) {
+export default function ApiKeyResetButton({ setDialogOpen }) {
     function handleClick() {
-        SetDialogOpen(true);
+        setDialogOpen(true);
     }
 
     return (
-        <Box>
+        <Box sx={{ mx: 2 }}>
             APIキーを再設定する
-            <KeyIcon
+            <IconButton
                 onClick={handleClick}
                 sx={{ color: 'text.primary' }}
-            />
+            >
+                <KeyIcon />
+            </IconButton>
         </Box>
     )
 }
